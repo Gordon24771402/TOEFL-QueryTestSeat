0267268c894617a79984a654fe3121bb318dbed9
Gordon24771402 <gordon24771402@gmail.com>
2020-09-13 09:04:44 +0800

    Update main.py

--- Source_Code/main.py
+++ Source_Code/main.py
@@ -26,20 +26,21 @@ while driver.current_url == 'https://toefl.neea.cn/login':
 citiesList = ["DONGGUAN", "SHENZHEN", "GUANGZHOU"]
 daysList = ["2020-09-26", "2020-09-27", "2020-10-10", "2020-10-14", "2020-10-17", "2020-10-24"]
 
-# Check Seat
+# Wait Until Fully Loaded
+time.sleep(5)
+driver.refresh()
+time.sleep(5)
+
+# Cheak Seat
 while True:
-    time.sleep(5)
-    driver.refresh()
-    time.sleep(5)
-    for i in range(5):
-        for city in citiesList:
-            for date in daysList:
-                js = 'return $.ajax({url: "testSeat/queryTestSeats",type: "GET",data: {' + f'city: "{city}",testDay: "{date}"' + '},dataType: "text",async: false,}).responseText;'
-                dataJSON = driver.execute_script(js)
-                if "true" in dataJSON:
-                    seatStatus = re.findall(r'<status>(\d+)</status>', dataJSON)
-                    if '1' in seatStatus:
-                        print('City: {} Date: {} Available'.format(city, date))
-                        while True:
-                            playsound('Alarm01.wav')
-        time.sleep(60)
+    for city in citiesList:
+        for date in daysList:
+            js = 'return $.ajax({url: "testSeat/queryTestSeats",type: "GET",data: {' + f'city: "{city}",testDay: "{date}"' + '},dataType: "text",async: false,}).responseText;'
+            dataJSON = driver.execute_script(js)
+            if "true" in dataJSON:
+                seatStatus = re.findall(r'<seatStatus>(\d+)</seatStatus>', dataJSON)
+                if '1' in seatStatus:
+                    print('City: {} Date: {} Available'.format(city, date))
+                    while True:
+                        playsound('Alarm01.wav')
+    time.sleep(60)
